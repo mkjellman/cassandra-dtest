5639580a6ebaea8e0c1d27c06b446026e7cc8184
Michael Kjellman <kjellman@apple.com>
2018-01-09 22:27:02 -0800

    Enable @flaky plugin for tests that failed due to ccm.node.TimeoutError. Add extra comments to test_failure_due_to_timeout function

--- dtest.py
+++ dtest.py
@@ -21,7 +21,7 @@ from cassandra.auth import PlainTextAuthProvider
 from cassandra.cluster import ExecutionProfile
 from cassandra.policies import RetryPolicy, RoundRobinPolicy
 from ccmlib.common import get_version_from_build, is_win
-from ccmlib.node import ToolError
+from ccmlib.node import ToolError, TimeoutError
 from distutils.version import LooseVersion
 
 
@@ -216,12 +216,27 @@ def cleanup_docker_environment_before_test_execution():
 def test_failure_due_to_timeout(err, *args):
     """
     check if we should rerun a test with the flaky plugin or not.
-    for now, only run if we got a cassandra.OperationTimedOut exception.
-    also, introduce a 2 second sleep before we re-run the test to give
-    some time for environmental issues to sort themselves to make
-    the re-run more likely to succeed
+    for now, only run if we failed the test for one of the following
+    three exceptions: cassandra.OperationTimedOut, ccm.node.ToolError,
+    and ccm.node.TimeoutError.
+
+    - cassandra.OperationTimedOut will be thrown when a cql query made thru
+    the python-driver times out.
+    - ccm.node.ToolError will be thrown when an invocation of a "tool"
+    (in the case of dtests this will almost always invoking stress).
+    - ccm.node.TimeoutError will be thrown when a blocking ccm operation
+    on a individual node times out. In most cases this tends to be something
+    like watch_log_for hitting the timeout before the desired pattern is seen
+    in the node's logs.
+
+    if we failed for one of these reasons - and we're running in docker - run
+    the same "cleanup" logic we run before test execution and test setup begins
+    and for good measure introduce a 2 second sleep. why 2 seconds? because it's
+    magic :) - ideally this gets the environment back into a good state and makes
+    the rerun of flaky tests likely to suceed if they failed in the first place
+    due to environmental issues.
     """
-    if issubclass(err[0], OperationTimedOut) or issubclass(err[0], ToolError):
+    if issubclass(err[0], OperationTimedOut) or issubclass(err[0], ToolError) or issubclass(err[0], TimeoutError):
         if running_in_docker():
             cleanup_docker_environment_before_test_execution()
             time.sleep(2)
